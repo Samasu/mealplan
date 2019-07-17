fe96b108c27a78c50ea9909bf53bc476feb7e09d
Sada <samasunari@gmail.com>
2019-07-18 01:45:37 +0900

    add test login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,21 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+    protect_from_forgery except: :sessions 
 
-    # ログイン済ユーザーのみにアクセスを許可する
-    
-  
-    # deviseコントローラーにストロングパラメータを追加する          
+             
     before_action :configure_permitted_parameters, if: :devise_controller?
   
     
 
     protected
+
+    def after_update_path_for(resource)
+      user_path(id: current_user.id)
+    end
+
+    def after_sign_in_path_for(resource)
+      plans_path
+    end
+
     def configure_permitted_parameters
       # サインアップ時にnameのストロングパラメータを追加
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name,])
